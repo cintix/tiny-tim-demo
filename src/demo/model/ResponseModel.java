775d3da3608e42abb0b42e2157f6131f72fd5ade
Michael Martinsen <migo@tv2.dk>
2021-06-07 07:23:47 +0200

    import

---
+++ src/demo/model/ResponseModel.java
@@ -0,0 +1,22 @@
+/*
+ */
+package demo.model;
+
+/**
+ *
+ * @author cix
+ */
+public class ResponseModel {
+
+    private final String message;
+
+    public ResponseModel(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseModel{" + "message=" + message + '}';
+    }
+
+}
