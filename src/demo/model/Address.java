a6686d3c98dbcf5a59aad08ab0405a45daf2a80e
Michael Martinsen <migo@tv2.dk>
2021-09-07 22:37:10 +0200

    diff

---
+++ src/demo/model/Address.java
@@ -0,0 +1,39 @@
+/*
+ */
+package demo.model;
+
+/**
+ *
+ * @author cix
+ */
+public class Address {
+
+    private int zip;
+    private String city;
+    private String addressLine;
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+}
--- src/demo/model/Person.java
+++ src/demo/model/Person.java
@@ -11,7 +11,7 @@ public class Person {
     private String name;
     private int pinCode;
     private String email;
-
+    
     public String getName() {
         return name;
     }
